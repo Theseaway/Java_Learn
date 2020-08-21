db88ced84da70c1f82df08c0aff312bb2b4273a9
Theseaway <2606621776@qq.com>
2020-08-21 09:59:09 +0800

    src/Test.java

---
+++ src/Test.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Test {
+  public static void main(String[] args){
+    String[] s = new String[]{"We", "are"};
+    System.out.println(Arrays.toString(s));
+  }
+}
